52e7ba6138867d96688ffcfe2e36ad7693c5bb70
Clovis Rodríguez <clovis1992@gmail.com>
2018-01-23 19:31:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var katzDeliLine = [];
 function takeANumber (katzDeliLine, customerName) {
   katzDeliLine.push(customerName)
   console.log(`Welcome ${customerName}. You are number ${katzDeliLine.length} in line`)
-  return `Welcome ${customerName}. You are number ${katzDeliLine.length} in line`
+  return `Welcome, ${customerName}. You are number ${katzDeliLine.length} in line`
 }
 
 function nowServing (katzDeliLine) {
   if (katzDeliLine.length > 0) {
     return katzDeliLine.splice(0,1)
   } else {
-    return 'There is nobody waiting to be served'
+    return 'There is nobody waiting to be served!'
   }
 }
 
